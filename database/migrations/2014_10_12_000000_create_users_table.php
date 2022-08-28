f8379b1e1a2ad838962d1a44d45cf96e1770c9a3
Lazuardi <lazuardi.reznnov@gmail.com>
2022-08-28 08:46:47 +0700

    setting formatter

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->string('name');
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('is_admin')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
